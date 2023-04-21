f5252c0d96b153bbe5d547d2ad302710c06fa1d2
Olami <tenyfash@yahoo.com>
2023-04-21 01:06:56 -0700

    My first code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,18 @@
-#include<stdio.h>
+/**
+ * My first ever code
+ * Done by Posi
+ */
 
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual puzzle."
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0); 
 }
