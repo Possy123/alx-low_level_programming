cb4aacd8e60585d5bc9db65debd5c3cbfccddb19
Olami <tenyfash@yahoo.com>
2023-04-21 02:36:55 -0700

    My second code on c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+/*
+ * File: printf.c
+ * Auth: Posi
+ */
+
+#include <stdio.h>
+
+/**
+ * main -Entry point of code
+ *
+ * main -prints "with proper grammar, but
+ *		the outcome is a piece of art"
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
