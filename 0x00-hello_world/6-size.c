1e1c6b982247db318aaba3d14f47fd79d1877c1a
Olami <tenyfash@yahoo.com>
2023-04-23 09:37:36 -0700

    size types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,11 +20,11 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("the size of a char: %lu byte(s)\n", sizeof(b));
-	printf("the size of an int: %lu byte(s)\n", sizeof(a));
-	printf("the size of a long int: %lu byte(s)\n", sizeof(c));
-	printf("the size of a long long int: %lu byte(s)\n", sizeof(d));
-	printf("the size of a float: %lu byte(s)\n", sizeof(e));
+	printf("Size of a char: %lu byte(s)\n", sizeof(b));
+	printf("Size of an int: %lu byte(s)\n", sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", sizeof(e));
 
 	return (0);
 }
