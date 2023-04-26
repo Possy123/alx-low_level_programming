0ac1eb7dc030de2f365bb6426c3228a13200979f
Possy123 <tenyfash@yahoo.com>
2023-04-26 19:53:53 +0100

    testing

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,16 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 	while (i <= 9)
-		i++;
+
 	{
 		int ch = 'a';
 		while (ch <= 'z')
+
 		{
 		_putchar(ch);
 		ch++;
 		}
+	i++;
 	_putchar('\n');
 	}
 }
