a9e58d1620c837b5e400d51d5b3f209721503dd6
Possy123 <tenyfash@yahoo.com>
2023-04-26 22:56:22 +0100

    lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,12 @@
 
 /**
  * _islower - is to print lowercase
- *
+ * c - is character
  * Return: can be 0 or 1.
  */
 int _islower(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
