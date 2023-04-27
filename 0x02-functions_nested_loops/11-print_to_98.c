3b56bf4dba1a9f22e5a5b2d374fa8a0758826367
Possy123 <tenyfash@yahoo.com>
2023-04-27 03:35:25 +0100

    prints number

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,29 +10,12 @@
  */
 void print_to_98(int n)
 {
-    int i;
-
-    if (n <= 98)
-    {
-        for (i = n; i < 98; i++)
-        {
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
-            putchar(',');
-            putchar(' ');
-        }
-    }
-    else
-    {
-        for (i = n; i > 97; i--)
-        {
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
-            putchar(',');
-            putchar(' ');
-        }
-    }
-    putchar('9');
-    putchar('8');
-    putchar('\n');
+	if (n < 98)
+	{
+		for (n = 0; n <= 97; n++)
+			printf("%d", n);
+	}
+	else
+		for (n = 0; n > 98; n--)
+			printf("%d", n);
 }
