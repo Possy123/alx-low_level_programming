6620ab5ca62e1386a941b9f0af3f19905f1cb038
Olami <tenyfash@yahoo.com>
2023-04-28 21:43:08 -0700

    numbers minus 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0-9 except 2 & 4
+ *
+ * Return: numbers
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a != '2' && a != '4')
+			putchar(a);
+	}
+	putchar('\n');
+}
