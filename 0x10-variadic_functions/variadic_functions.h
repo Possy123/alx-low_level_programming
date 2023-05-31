25cf69b571cce5549feb520e07f708f866d9a5df
Olami <tenyfash@yahoo.com>
2023-05-30 22:45:10 -0700

    updated

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
@@ -10,5 +11,13 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
+
+void printchar(char *sep, va_list func);
+
+void printint(char *sep, va_list func);
+
+void printfloat(char *sep, va_list func);
+
+void printstring(char *sep, va_list func);
 #endif /* VARIADIC_FUNCTIONS_H */
 
