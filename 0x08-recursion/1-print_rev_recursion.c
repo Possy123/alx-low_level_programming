568587b21146e825535db344b427fcb48ab58632
Possy123 <tenyfash@yahoo.com>
2023-06-07 13:07:38 -0700

    prints string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - prints strings in reverse recursion
+ * @s: string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+}
+
