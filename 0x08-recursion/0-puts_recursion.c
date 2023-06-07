4b14754d94de144b7794630162552a44f4bc10d4
Possy123 <tenyfash@yahoo.com>
2023-06-07 12:43:13 -0700

    prints string with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints out recursion
+ * *s: string
+ * Return: string
+ */
+void _puts_recursion(char *s)
+{
+	if(*s == '\0')/* string is null characte i.e end*/
+	{
+		putchar('\n');/* print a new line*/
+	}
+	else
+	{
+		putchar(*s);/* print the first letter of the string*/
+		_puts_recursion(s + 1);/* keep printing the next letter*/
+	}
+}
+
