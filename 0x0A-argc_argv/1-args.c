30c319409883429c96055d6865862d0bb3f6ee91
Possy123 <tenyfash@yahoo.com>
2023-06-14 13:57:47 -0700

    to count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the name of the function
+ * @argc: it is used to count command line argument
+ * @argv: the variables in a command line argument
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
