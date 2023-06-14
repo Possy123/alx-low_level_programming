f0917bcbfe3a1b1cf6dfb93bf3303abe9f627cd0
Possy123 <tenyfash@yahoo.com>
2023-06-14 14:53:27 -0700

    work in progress

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: counts argument
+ * @argv: prints variables
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int j = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		while (i < argc)
+			char *argv = argv[i];
+		while (
