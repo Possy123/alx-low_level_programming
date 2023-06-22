6585f5e631c948b8ec08d71e0b38756060a30592
Possy123 <tenyfash@yahoo.com>
2023-06-21 17:22:18 -0700

    arrays

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function name
+ * @array: interger
+ * @size: size of array
+ * @action: pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+		size_t i;
+
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+}
