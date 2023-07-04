ea8037c6724243dfdfb1a5a3957d92977be54392
Possy123 <tenyfash@yahoo.com>
2023-07-03 18:56:13 -0700

    add note

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -23,12 +23,15 @@ list_t *add_node_end(list_t **head, const char *str)
 	node->next = NULL;
 
 	if (*head == NULL)
+	{
 		*head = node;
+	}
 	else
+	{
 		temp = *head;
 	while (temp->next != NULL)
 		temp = temp->next;
 	temp->next = node;
-
+	}
 	return (node);
 }
