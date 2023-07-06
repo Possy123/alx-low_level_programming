fd3106c98cc06652c6804d1ea6c338541ce36421
Possy123 <tenyfash@yahoo.com>
2023-07-06 01:06:33 -0700

    addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "main.h"
+#include <ctype.h>
 /**
  * main - main function
  * @argc: counts argument
@@ -11,14 +12,25 @@ int main(int argc, char *argv[])
 {
 	int i = 1;
 	int j = 0;
+	int sum = 0;
 
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	else
-	{
-		while (i < argc)
-			char *argv = argv[i];
-		while (
+		for (; i < argc; i++)
+		{
+			for (;argv[i][j] != '\0'; j++)
+			{
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+		return (0);
+}
