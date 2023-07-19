7984a25743d67dee84240bdf58b7f589e6e40436
Possy123 <tenyfash@yahoo.com>
2023-07-18 20:12:14 -0700

    append

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (fp == NULL)
+	if (fp == NULL || text_content == NULL)
 	{
 		write(1, "fails", 6);
 		return (-1);
