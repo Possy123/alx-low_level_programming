c33c626c22455d7739b7277f81d2672986f04976
Possy123 <tenyfash@yahoo.com>
2023-10-30 12:58:35 -0700

    question 1

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 void copy_file(const char *file_from, const char *file_to);
-void display_elf_header_info(const char *elf_filename);
-void print_elf_header_info(Elf64_Ehdr *header);
+/*void display_elf_header_info(const char *elf_filename);*/
+/*void print_elf_header_info(Elf64_Ehdr *header);*/
 #endif
